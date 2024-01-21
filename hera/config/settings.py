f59a059b7196d087128eaf782d6c19d8c175abf5
DgUcab <dgucab@gmail.com>
2024-01-22 00:36:18 +0800

    Test Bad request fix

--- hera/config/settings.py
+++ hera/config/settings.py
@@ -33,7 +33,7 @@ DEBUG = False
 ALLOWED_HOSTS = ['localhost','127.0.0.1','192.168.1.40','https://hera-tni7.onrender.com']
 
 CSRF_TRUSTED_ORIGINS = [
-  'https://deployedurlhere.com'
+  'https://hera-tni7.onrender.com'
 ]
 # Application definition
 
